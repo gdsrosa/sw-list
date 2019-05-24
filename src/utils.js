f3dbe13a480b7801838a75a20ec62ef09b634340
gdsrosa <gabrieldsrosa@gmail.com>
2019-05-24 17:20:47 -0300

    (+) Add utils file
    This commit will add a utils file that helps to handle API fetch of starships and
    characters.

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+import axios from 'axios';
+
+export const getPeople = (url, people, resolve, reject) => {
+  axios
+    .get(url)
+    .then(response => {
+      const retrivedPeople = people.concat(response.data.results);
+      if (response.data.next !== null) {
+        getPeople(response.data.next, retrivedPeople, resolve, reject);
+      } else {
+        resolve(retrivedPeople);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      reject('Something wrong. Please refresh the page and try again.');
+    });
+};
+
+export const getStarships = (url, starhips, resolve, reject) => {
+  axios
+    .get(url)
+    .then(response => {
+      const retrivedStarships = starhips.concat(response.data.results);
+      if (response.data.next !== null) {
+        getStarships(response.data.next, retrivedStarships, resolve, reject);
+      } else {
+        resolve(retrivedStarships);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      reject('Something wrong. Please refresh the page and try again.');
+    });
+};
