a1c14188aaacfe27f3ecaa28bd4b863fa961de0c
gdsrosa <gabrieldsrosa@gmail.com>
2019-05-26 10:56:28 -0300

    (-) Remove redux dev tools config
    This commmit will remove the redux devtools setup as it was causing
    issues when loading the application in the browser.

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,13 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { charactersReducer } from '../ducks/characters';
 import { starshipsReducer } from '../ducks/starships';
-
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const rootReducer = combineReducers({
   charactersReducer,
   starshipsReducer,
 });
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(ReduxThunk),
-    reduxDevTools
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default store;
